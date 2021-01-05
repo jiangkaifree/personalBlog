9e775e40068092aa2ec3f937cf3c946f9d04a509
2462870727@qq.com <2462870727@qq.com>
2021-01-05 18:55:01 +0800

    修改菜单栏

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,17 @@
 /*
  * @Author: jk
  * @Date: 2020-12-14 10:57:56
- * @Last Modified by: jk
- * @Last Modified time: 2021-01-04 17:05:20
+ * @Last Modified by: 小菜鸡
+ * @Last Modified time: 2021-01-05 17:40:04
  */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import QueueAnim from "rc-queue-anim";
 import { Layout, Menu, Breadcrumb, Avatar } from "antd";
 import {
   EditOutlined,
   AudioOutlined,
-  PieChartOutlined,
-  SettingOutlined,
   UserOutlined,
   OrderedListOutlined,
   HomeOutlined,
@@ -26,91 +24,102 @@ import Routes from "./router";
 import Login from "./views/Login/Login";
 import AddArticle from "./views/AddArticle/AddArticle"; // 添加文章页面
 import NotFound from "./views/NotFound/NotFound"; //404
-import ResultPage from "./views/ResultPage/ResultPage";     // 结果页面
+import ResultPage from "./views/ResultPage/ResultPage"; // 结果页面
 
 // import Data from "./views/Data/Data";
 // import AddArticle from "./views/AddArticle/AddArticle";
 
 function App(props) {
   const { Content, Footer, Sider } = Layout;
-  const { SubMenu } = Menu;
   const [collapsed, setCollapsed] = useState(false);
-  const [activeItem, setActiveItem] = useState(["data"]); // 路由
-  const [breadcrumbItem, setBreadcrumbItem] = useState("实时数据"); // 面包屑导航
+  const [activeItem, setActiveItem] = useState([props.location.pathname]); // 路由
+  const [breadcrumbItem, setBreadcrumbItem] = useState("可视化数据"); // 面包屑导航
   // console.log(props.history.listen(route => {
   //   console.log(route)
   // }),'props')
+  // const menuList = [
+  //   {
+  //     path: "data",
+  //     title: "数据台",
+  //     icon: <PieChartOutlined />,
+  //     children: [
+  //       {
+  //         path: "data",
+  //         title: "实时数据",
+  //         icon: <RadarChartOutlined />,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: "",
+  //     title: "文章管理",
+  //     icon: <SettingOutlined />,
+  //     children: [
+  //       {
+  //         path: "/admin/addArticle",
+  //         title: "添加文章",
+  //         icon: <EditOutlined />,
+  //       },
+  //       {
+  //         path: "articlesList",
+  //         title: "文章列表",
+  //         icon: <OrderedListOutlined />,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: "chat",
+  //     title: "留言管理",
+  //     icon: <AudioOutlined />,
+  //     children: [
+  //       {
+  //         path: "/admin/chat",
+  //         title: "留言管理",
+  //         icon: <AudioOutlined />,
+  //       },
+  //     ],
+  //   },
+  // ];
+
   const menuList = [
     {
-      path: "data",
-      title: "数据台",
-      icon: <PieChartOutlined />,
-      children: [
-        {
-          path: "data",
-          title: "实时数据",
-          icon: <RadarChartOutlined />,
-        },
-      ],
+      path: "/admin/data",
+      title: "可视化数据",
+      icon: <RadarChartOutlined />,
     },
     {
-      path: "",
-      title: "文章管理",
-      icon: <SettingOutlined />,
-      children: [
-        {
-          path: "/admin/addArticle",
-          title: "添加文章",
-          icon: <EditOutlined />,
-        },
-        {
-          path: "articlesList",
-          title: "文章列表",
-          icon: <OrderedListOutlined />,
-        },
-      ],
+      path: "/admin/addArticle",
+      title: "添加文章",
+      icon: <EditOutlined />,
     },
     {
-      path: "chat",
+      path: "/admin/articlesList",
+      title: "文章列表",
+      icon: <OrderedListOutlined />,
+    },
+    {
+      path: "/admin/chat",
       title: "留言管理",
       icon: <AudioOutlined />,
-      children: [
-        {
-          path: "/admin/chat",
-          title: "留言管理",
-          icon: <AudioOutlined />,
-        },
-      ],
     },
   ];
 
-  // 时间输出
-  // const time = () => {
-  //   const time = "";
-  //   if (new Date().getHours() >= 9) {
-  //     time = "早上好！";
-  //   } else if (new Date().getHours() >= 12) {
-  //     time = "上午好！";
-  //   } else if (new Date().getHours() >= 18) {
-  //     time = "下午好";
-  //   } else if (new Date().getHours() >= 24) {
-  //     time = "晚上好！！";
-  //   } else {
-  //     time = "凌晨早！";
-  //   }
-  //   return (<span>{time}</span>);
-  // };
+  useEffect(() => {
+    const titleList = {
+      "/admin/data": "可视化数据",
+      "/admin/articlesList": "文章列表",
+      "/admin/chat": "留言管理",
+    };
+    // setActiveItem(['1']);
+    setBreadcrumbItem(titleList[props.location.pathname]);
+
+  }, [props.location.pathname]);
 
   // 跳转页面
   const setPage = (key) => {
     console.log(key);
-    setActiveItem([key]);
-    props.history.push(key);
-  };
-
-  // 设置面包屑title
-  const changeBreadcrumbItem = ({ key }) => {
-    // console.log(menuList,key)
+    setActiveItem([menuList[key].path]);
+    props.history.push(menuList[key].path);
     setBreadcrumbItem(menuList[key].title);
   };
 
@@ -119,13 +128,13 @@ function App(props) {
       <Switch>
         <Route path="/user/login" exact component={Login}></Route>
         <Redirect to="/user/login" from="/" exact></Redirect>
-        <Route  path="/admin/addArticle/:id?" component={AddArticle}></Route>
+        <Route path="/admin/addArticle/:id?" component={AddArticle}></Route>
         <Route exact path="/admin/result" component={ResultPage}></Route>
         <Redirect to="/admin/data" from="/admin" exact></Redirect>
         <Route path="/admin">
           <Layout className={styles.layoutWrap}>
             <Sider
-              theme="light"
+              theme="dark"
               collapsible
               collapsed={collapsed}
               onCollapse={(collapsed) => setCollapsed(collapsed)}
@@ -134,23 +143,19 @@ function App(props) {
               <QueueAnim delay={550} type="left" leaveReverse>
                 <div className={styles.avatarWrap} key="0">
                   <Avatar size={48} icon={<UserOutlined />} />
-                  <p className={styles.date}>江凯</p>
+                  <p className={styles.name}>江凯</p>
                   <p className={styles.date}>
                     {new Date().toLocaleDateString()}
                   </p>
                 </div>
                 <Menu
-                  theme="light"
+                  className={styles.menuWrap}
                   defaultSelectedKeys={activeItem}
                   mode="inline"
                   onClick={({ key }) => setPage(key)}
                   key="1"
                 >
-                  {menuList.map((item, index) => (
-                    //   <Menu.Item key={Item.path}>
-                    //   {item.icon}
-                    //   <span>{item.title}</span>
-                    // </Menu.Item>
+                  {/* {menuList.map((item, index) => (
 
                     <SubMenu
                       key={index}
@@ -169,6 +174,11 @@ function App(props) {
                         </Menu.Item>
                       ))}
                     </SubMenu>
+                  ))} */}
+                  {menuList.map((item, index) => (
+                    <Menu.Item key={index} icon={item.icon}>
+                      {item.title}
+                    </Menu.Item>
                   ))}
 
                   {/* <Menu.Item key="data">
@@ -224,7 +234,7 @@ function App(props) {
           </Layout>
         </Route>
 
-          {/* 暂时注释 */}
+        {/* 暂时注释 */}
         <Route path="/404" component={NotFound} />
         <Redirect from="*" to="/404" />
       </Switch>

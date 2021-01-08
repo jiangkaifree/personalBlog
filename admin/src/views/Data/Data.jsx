76c219c3c90214a6ab422339022c717bc3cdbbfb
2462870727@qq.com <2462870727@qq.com>
2021-01-08 19:39:27 +0800

    增加后台环境

--- admin/src/views/Data/Data.jsx
+++ admin/src/views/Data/Data.jsx
@@ -3,11 +3,11 @@
 // import styles from "./Data.module.scss";
 // import { getArticleList } from "../../utils/api";
 const Data = () => {
-  
+  // console.log(process.env)
   return (
     <div>
       数据页面
-      
+      <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" />
     </div>
   );
 };

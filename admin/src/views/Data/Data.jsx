535a2033a1dd910a08e135fefe90d792cc082334
2462870727@qq.com <2462870727@qq.com>
2021-01-19 19:01:42 +0800

    提交可视化页面数据

--- admin/src/views/Data/Data.jsx
+++ admin/src/views/Data/Data.jsx
@@ -1,23 +1,67 @@
-// import { Button } from "antd";
-// import React from "react";
-// import styles from "./Data.module.scss";
-// import { getArticleList } from "../../utils/api";
+import { useState, useEffect } from "react";
+import { Area } from "@ant-design/charts";
 const Data = () => {
   // console.log(process.env)
+  
+  const config = {
+    data: [
+      {
+        weekDay: '星期一',
+        viewCounts: 20,
+      },
+      {
+        weekDay: '星期二',
+        viewCounts: 140,
+      },
+      {
+        weekDay: '星期三',
+        viewCounts: 120,
+      },
+      {
+        weekDay: '星期四',
+        viewCounts: 86,
+      },
+      {
+        weekDay: '星期五',
+        viewCounts: 89,
+      },
+      {
+        weekDay: '星期六',
+        viewCounts: 75,
+      },
+      {
+        weekDay: '星期日',
+        viewCounts: 100,
+      },
+    ],
+    xField: "weekDay",
+    yField: "viewCounts",
+    xAxis: { tickCount: 12 },
+    areaStyle: function areaStyle() {
+      return {
+        fill: "l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff",
+        shadowColor: "#1890ff",
+        shadowBlur: 10,
+      };
+    },
+  };
   return (
     <>
-    <div>
-      数据页面
-      <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" />
-    </div>
-     <style jsx>
-     {`
-       div {
-         color: red;
-       }
-     `}
-   </style>
-   </>
+      <div className='mainWrap'>
+        <div className='div'>
+        <Area {...config} />
+        </div>
+      </div>
+      <style jsx>{`
+        .mainWrap{
+           background-color: #333;
+           font-size: 
+        }
+        .mainWrap > .div{
+          background-color: #fff
+        }
+      `}</style>
+    </>
   );
 };
 

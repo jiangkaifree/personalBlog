6a9933011b0669bcf63f5eeab836b3360eb85131
2462870727@qq.com <2462870727@qq.com>
2021-01-20 18:33:01 +0800

    提交表格

--- admin/src/views/Data/Data.jsx
+++ admin/src/views/Data/Data.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
-import { TinyArea  } from "@ant-design/charts";
+import { TinyArea ,Progress  } from "@ant-design/charts";
 import {QuestionOutlined} from '@ant-design/icons'
 import styles from "./Data.module.scss";
 const Data = () => {
   // console.log(process.env)
-
+  const todayConfig = {
+    barWidthRatio: 0.3,
+    autoFit: true,
+    percent: 0.7,
+    color: ['#5B8FF9', '#E8EDF3'],
+  };
   const totalConfig = {
     data: [
       30,55,22,43,13,43,44,
@@ -43,6 +48,9 @@ const Data = () => {
             <span>日同比 <span className={styles.rate}>12% </span></span>
             <span>周同比 <span className={styles.rate}>11% </span></span>
           </p>
+          <div>
+          <Progress {...todayConfig} />
+          </div>
         </div>
        
       </div>
